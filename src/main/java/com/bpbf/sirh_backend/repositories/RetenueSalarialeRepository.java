a3e555c780a3f8de773b562da7fb62ceff1ba6cb
davidzoro9 <118844619+davidzoro9@users.noreply.github.com>
2026-08-07 14:16:17 +0000

    Fix_repositories_queries_for_ManyToOne

--- src/main/java/com/bpbf/sirh_backend/repositories/RetenueSalarialeRepository.java
+++ src/main/java/com/bpbf/sirh_backend/repositories/RetenueSalarialeRepository.java
@@ -2,11 +2,14 @@ package com.bpbf.sirh_backend.repositories;
 
 import com.bpbf.sirh_backend.entities.RetenueSalariale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface RetenueSalarialeRepository extends JpaRepository<RetenueSalariale, Long> {
-    List<RetenueSalariale> findByEmployeeId(Long employeeId);
+    @Query("SELECT r FROM RetenueSalariale r WHERE r.employee.id = :employeeId")
+    List<RetenueSalariale> findByEmployeeId(@Param("employeeId") Long employeeId);
 }
