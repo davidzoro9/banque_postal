181ad0608edfc3541dab5ea5f2563bfddb2608b1
davidzoro9 <118844619+davidzoro9@users.noreply.github.com>
2026-08-19 10:37:05 +0000

    feat(backend): port 8090 et fix Hibernate id mapping sur Employee

--- src/main/java/com/bpbf/sirh_backend/mappers/EmployeeMapper.java
+++ src/main/java/com/bpbf/sirh_backend/mappers/EmployeeMapper.java
@@ -3,6 +3,7 @@ package com.bpbf.sirh_backend.mappers;
 import com.bpbf.sirh_backend.dtos.EmployeeDto;
 import com.bpbf.sirh_backend.entities.Employee;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import java.util.List;
@@ -12,5 +13,7 @@ public interface EmployeeMapper {
     EmployeeDto toDto(Employee entity);
     Employee toEntity(EmployeeDto dto);
     List<EmployeeDto> toDtos(List<Employee> entities);
+    
+    @Mapping(target = "id", ignore = true)
     void updateEntityFromDto(EmployeeDto dto, @MappingTarget Employee entity);
 }
